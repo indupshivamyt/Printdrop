3d5305aa5f58c02a165908155ed4a61eafde726a
indupshivamyt <ind.up.shivamyt@gmail.com>
2025-07-19 22:17:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const faders = document.querySelectorAll('.fade');
+const appearOptions = {
+  threshold: 0.2,
+  rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add('visible');
+    appearOnScroll.unobserve(entry.target);
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
